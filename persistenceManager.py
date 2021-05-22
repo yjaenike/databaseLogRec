909533dc4f4e0cfc7bedd86a3d2a2ab0dd4bed24
janik-hasche <janikhasche@googlemail.com>
2021-05-22 12:47:34 +0200

    Make PersistanceManger singleton and add dummmy transaction functions

--- persistenceManager.py
+++ persistenceManager.py
@@ -1,12 +1,43 @@
 import os
 
-class PersistenceManager():
 
-    #def __init__(self):
+class PersistenceManager:
+    _instance = None
+
+    test = 5
+
+    def __new__(cls):
+        if cls._instance is None:
+            print('Creating the object')
+            cls._instance = super(PersistenceManager, cls).__new__(cls)
+            # Put any initialization here.
+        return cls._instance
+
+    def begin_transaction(self) -> int:
+        """
+        :return: transaction id
+        """
+        return 0
+
+    def commit(self, ta_id: int) -> bool:
+        """
+        :param ta_id: transaction id
+        :return: true if successful
+        """
+        pass
+
+    def write(self, ta_id: int, page_id: int, data: str):
+        """
+        :param ta_id: transaction id
+        :param page_id: page id
+        :param data: user data
+        :return: None
+        """
+        pass
 
     def write_data_to_file(self, lsn, pageid, data):
         try:
-            f = open('pages/page_{}.txt'.format(pageid),"w")
+            f = open('pages/page_{}.txt'.format(pageid), "w")
             f.write(data)
             f.close()
         except FileNotFoundError:
@@ -18,10 +49,15 @@ class PersistenceManager():
             # do stuff with log here
             f.close()
         except FileNotFoundError:
-            print('File does not exist')        
+            print('File does not exist')
+
 
 def main():
     pm = PersistenceManager()
+    pm.test = 6;
+    pm2 = PersistenceManager()
+    print(pm2.test)
+
 
 if __name__ == "__main__":
     main()
