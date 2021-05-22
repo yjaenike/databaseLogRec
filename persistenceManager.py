030cb756ad26813602f74e05336c8976e36d17d9
yjaenike <yannicjae@gmail.com>
2021-05-22 12:29:29 +0200

    last changes

--- persistenceManager.py
+++ persistenceManager.py
@@ -4,7 +4,7 @@ class PersistenceManager():
 
     #def __init__(self):
 
-    def write_to_file(self, lsn, pageid, data):
+    def write_data_to_file(self, lsn, pageid, data):
         try:
             f = open('pages/page_{}.txt'.format(pageid),"w")
             f.write(data)
@@ -12,12 +12,16 @@ class PersistenceManager():
         except FileNotFoundError:
             print('File does not exist')
 
-        
+    def write_log_to_file(self, lsn, taid, pageid, data):
+        try:
+            f = open('filename')
+            # do stuff with log here
+            f.close()
+        except FileNotFoundError:
+            print('File does not exist')        
 
 def main():
     pm = PersistenceManager()
-
-    pm.write_to_file(2,2,"hello earth")
 
 if __name__ == "__main__":
     main()
